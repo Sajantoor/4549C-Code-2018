72e035ed94aca00bf08bad93f59eae5970f5360d
Sajan Toor <sajantoor98@gmail.com>
2018-10-02 09:41:23 -0700

    Added dual motor support

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -36,27 +36,21 @@ using namespace pros::literals;
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	auto left_mtr = 1_mtr;
-	pros::Motor right_mtr(2);
+	auto left_mtr2 = 2_mtr;
+	pros::Motor right_mtr(3);
+	pros::Motor right_mtr2(4);
 	while (true) {
 		int left = master.get_analog(ANALOG_LEFT_Y);
 		//	int leftText = (left / 127) * 100;
 		pros::lcd::set_text(1, "Left speed: " + std::to_string(left));
 		int right = master.get_analog(ANALOG_RIGHT_Y);
 		//	int rightText = (right / 127) * 100;
-		pros::lcd::set_text(2, "Right speed: " + std::to_string(left));
-
-		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
-		 	right_mtr = 127;
-			pros::delay(20);
-		}
-
-		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
-			left_mtr = 127;
-			pros::delay(20);
-		}
-
+		pros::lcd::set_text(2, "Right speed: " + std::to_string(right));
 		left_mtr = left;
+		left_mtr2 = (left * -1);
+
 		right_mtr = (right * -1);
+		right_mtr2 = right;
 		pros::delay(20);
 	}
 }
