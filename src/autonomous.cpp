1714f8550a0795a8227d86654ed72f57b9089409
Sajan Toor <sajandeep.toor@surreyschools.ca>
2018-11-01 10:41:37 -0700

    Some changes

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,7 +1,29 @@
 #include "main.h"
+// Motors
+pros::Motor left_mtr(9);
+pros::Motor forward_left_mtr(2);
+pros::Motor right_mtr(3);
+pros::Motor forward_right_mtr2(4);
+pros::Motor crane(5);
+pros::Motor launcher(6);
+pros::Motor launcher2(7);
 
 int autonomousMode = 0;
 
+void moveMotors(int left, int right, int delay) {
+  left_mtr = left;
+  forward_left_mtr = left;
+  right_mtr = right;
+  forward_right_mtr2 = right;
+  pros::delay(delay);
+}
+
 void autonomous() {
-  
+  while (autonomousMode == 0) {
+    int left = 127;
+    int right = 127;
+    int delay = 3000;
+    moveMotors(left, right, delay);
+    pros::delay(20);
+  }
 }
