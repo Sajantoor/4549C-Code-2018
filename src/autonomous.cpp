398f2c865978d59c4eaa9fa2b3b6f31141ba0b34
Sajan Toor <sajandeep.toor@surreyschools.ca>
2018-11-16 13:17:02 -0800

    Motors actually work this time

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -8,32 +8,32 @@ pros::Motor craneAuto(5);
 pros::Motor launcherAuto(6);
 pros::Motor launcher2Auto(7);
 
-int autonomousMode = 1;
-
-void moveMotors(int left, int right, int delay) {
-  left_mtrAuto = left;
-  forward_left_mtrAuto = left;
-  right_mtrAuto = right;
-  forward_right_mtrAuto = right;
-  pros::delay(delay);
-  autonomousMode = 2;
-}
-
+// int autonomousMode = 1;
+//
+// void moveMotors(int left, int right, int delay) {
+//   left_mtrAuto = left;
+//   forward_left_mtrAuto = left;
+//   right_mtrAuto = right;
+//   forward_right_mtrAuto = right;
+//   pros::delay(delay);
+//   autonomousMode = 1;
+// }
+//
 void autonomous() {
-  while (autonomousMode == 0) {
-    int left = 127;
-    int right = 127;
-    int delay = 3000;
-    moveMotors(left, right, delay);
-    pros::delay(20);
+//   while (autonomousMode == 0) {
+//     int left = -127;
+//     int right = 127;
+//     int delay = 3000;
+//     moveMotors(left, right, delay);
+//     pros::delay(20);
   }
-
-  // Testing if the motors themselves work
-  while (autonomousMode == 1) {
-    left_mtrAuto = 127;
-    forward_left_mtrAuto = 127;
-    right_mtrAuto = 127;
-    forward_right_mtrAuto = 127;
-    pros::delay(1000);
-  }
-}
+//
+//   // Testing if the motors themselves work
+//   while (autonomousMode == 1) {
+//     left_mtrAuto = -127;
+//     forward_left_mtrAuto = -127;
+//     right_mtrAuto = 127;
+//     forward_right_mtrAuto = 127;
+//     pros::delay(1000);
+//   }
+// }
--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,60 +1,60 @@
 #include "main.h"
-
 using namespace pros::literals;
 
 Controller master(CONTROLLER_MASTER);
-Motor left_mtr(9);
-Motor forward_left_mtr(2);
-Motor right_mtr(3);
-Motor forward_right_mtr(4);
+Motor left_mtr(3);
+Motor forward_left_mtr(4);
+Motor right_mtr(9);
+Motor forward_right_mtr(2);
 Motor crane(5);
 Motor launcher(6);
 Motor launcher2(7);
 Motor intake(8);
 int inverted = 0;
 
+
+int autonomousMode = 1;
+
+void moveMotors(int left, int right, int delay) {
+  left_mtr = left;
+  forward_left_mtr = left;
+  right_mtr = right;
+  forward_right_mtr = right;
+  pros::delay(delay);
+	pros::delay(1000);
+  autonomousMode = 1;
+}
+
+void autonomousClick() {
+  while (autonomousMode == 0) {
+    int left = 127;
+    int right = -127;
+    int delay = 3000;
+    moveMotors(left, right, delay);
+    pros::delay(20);
+  }
+
+  // Testing if the motors themselves work
+  while (autonomousMode == 1) {
+    left_mtr = 127;
+    forward_left_mtr = 127;
+    right_mtr = -127;
+    forward_right_mtr = -127;
+    pros::delay(1000);
+  }
+}
+
 void opcontrol() {
 	while (true) {
-		int left = master.get_analog(ANALOG_LEFT_Y);
+		int left = (master.get_analog(ANALOG_LEFT_Y));
 		lcd::set_text(1, "Left speed: " + std::to_string(left));
 		int right = (master.get_analog(ANALOG_RIGHT_Y) * -1);
 		lcd::set_text(2, "Right speed: " + std::to_string(right));
 		// Stick variables
-		if (inverted == 0) {
-			left_mtr = left;
-			forward_left_mtr = left;
-			right_mtr = right;
-			forward_right_mtr = right;
-		}
-
-		// if (inverted == 1) {
-		// 	// Gets X position of the sticks
-		// 	int leftX = master.get_analog(ANALOG_LEFT_X);
-		// 	int rightX = master.get_analog(ANALOG_RIGHT_X);
-		// 	// If the x position is greater than the Y position, then don't reverse
-		// 	if (leftX < 0) {
-		// 		leftX = (leftX * -1);
-		// 	}
-		//
-		// 	if (rightX < 0) {
-		// 		rightX = (rightX * -1);
-		// 	}
-		//
-		// 	if (leftX < left) {
-		// 		left_mtr = left;
-		// 	} else {
-		// 		left_mtr = (left * -1);
-		// 	}
-		//
-		// 	if (rightX < right) {
-		// 		right_mtr = right;
-		// 		forward_right_mtr = right;
-		// 	} else {
-		// 		right_mtr = (right * -1);
-		// 		forward_right_mtr = (right * -1);
-		// 	}
-		// }
-		//
+		left_mtr = left;
+		forward_left_mtr = left;
+		right_mtr = right;
+		forward_right_mtr = right;
 		// Reset button functions
 		crane = 0;
 		launcher = 0;
@@ -83,14 +83,8 @@ void opcontrol() {
 			delay(20);
 		}
 
-		// if (master.get_digital_new_press(DIGITAL_L1)) {
-		// 	if (inverted == 0) {
-		// 		inverted = 1;
-		// 	} else {
-		// 		inverted = 0;
-		// 	}
-		// 	pros::delay(20);
-		// }
-
+		if (master.get_digital(DIGITAL_X)) {
+			autonomousClick();
+		}
 	}
 }
