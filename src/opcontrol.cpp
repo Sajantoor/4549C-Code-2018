adab220ec4f614c1668dc05962aaf97f81d1b274
Sajan Toor <sajandeep.toor@surreyschools.ca>
2018-11-20 09:47:49 -0800

    Autonomous Changes

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -12,25 +12,104 @@ Motor launcher2(7);
 Motor intake(8);
 int autonomousMode = 1;
 
-void moveMotors(int left, int right, int delay) {
+void motors(int left, int forLeft, int right, int forRight, float timeDelay) {
+  // To save 4 - 5 lines and make the code more efficient.
   left_mtr = left;
-  forward_left_mtr = left;
+  forward_left_mtr = forLeft;
   right_mtr = right;
-  forward_right_mtr = right;
-  delay(delay);
-	delay(1000);
-  autonomousMode = 1;
+  forward_right_mtr = forRight;
+  delay(timeDelay);
 }
 
-void autonomousClick() {
-  while (autonomousMode == 0) {
-    int left = 127;
-    int right = 127;
-    int delay = 3000;
-    moveMotors(left, right, delay);
-    delay(2000);
-    autonomousMode = 1
+void rotate(float deg) {
+  // Rotation uses a center point of rotation
+  // The amount to move one degree while turning (in mileseoncs)
+  float degTime = 3.37;
+  float rotationTime = degTime * deg;
+  // If the degrees are negative, the left motor will be given positive power.
+  // If the degress are postive, the right motor will be given postive power.
+  if (rotationTime < 0) {
+    motors(127, 127, -127, -127, rotationTime);
+  } else {
+    rotationTime = (rotationTime * -1);
+    left_mtr = 127;
+    forward_left_mtr = 127;
+    right_mtr = -127;
+    forward_right_mtr = -127;
+    delay(rotationTime);
   }
+}
+
+void moveMotors(float squares) {
+  // Velocity is mileseconds to travel one square.
+  pros::lcd::set_text(2, "Function is working");
+  float velocity = 505.625;
+  float movementTime = squares * velocity;
+  pros::delay(20);
+  // time can't be negative
+  if (movementTime > 0) {
+    movementTime = (movementTime * -1);
+    pros::delay(20);
+  }
+  // Forward and back movment with the appropriate time delay
+  if (squares > 0) {
+    motors(127, 127, -127, -127, movementTime);
+  } else {
+    motors(-127, -127, 127, 127, movementTime);
+  }
+}
+
+
+// void turn(float deg) {
+//   // this function turns the motor, it uses a different point of rotation, different from the center point that rotation uses.
+//   // float degTime = ???
+//   float turnTime = degTime * deg;
+//
+//   if (turnTime < 0) {
+//     left_mtr = 127;
+//     forward_left_mtr = 127;
+//     right_mtr = -127;
+//     forward_right_mtr = -127;
+//     delay(turnTime);
+//   } else {
+//     turnTime = (turnTime * -1);
+//     left_mtr = 127;
+//     forward_left_mtr = 127;
+//     right_mtr = -127;
+//     forward_right_mtr = -127;
+//     delay(turnTime);
+//   }
+// }
+
+void shoot() {
+
+  // This function shoots the ball by given power to the spinner while preping the flywheel
+}
+
+// void inTake() {
+//   intake = 127;
+//   delay(?);
+//   // this function intakes the ball by spinning the motor
+// }
+//
+// void arm(int value) {
+//   if (value == 1) {
+//     // lift arm at the 90 degs position
+//     crane = 110;
+//     delay(?);
+//   }
+//
+//   if (value == 0) {
+//     crane = -110;
+//     delay(?);
+//   }
+// }
+
+
+void autonomousClick() {
+  moveMotors(1.0);
+  pros::lcd::set_text(1, "autonomous Function is working");
+  pros::delay(20);
 }
 
 void opcontrol() {
@@ -70,8 +149,9 @@ void opcontrol() {
 			delay(20);
 		}
 
-		if (master.get_digital(DIGITAL_X)) {
+		if (master.get_digital_new_press(DIGITAL_X)) {
 			autonomousClick();
+      pros::lcd::set_text(3, "Button is working!");
 		}
 	}
 }
