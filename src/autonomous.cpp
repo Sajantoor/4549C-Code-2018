ef621d0dc915d414537b03c98ac52bd7a5a47a9a
Sajan Toor <sajandeep.toor@surreyschools.ca>
2019-02-21 19:11:23 -0800

    Front auto done

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -2,22 +2,27 @@
 #include "variables.h"
 using namespace pros::literals;
 
-void shoot() {
+void shoot(int shootDelay, int intakeTime) {
   launcher = 110;
   launcher2 = -110;
-  delay(2000);
-  intake = 100;
-  delay(1000);
+  delay(shootDelay);
+  intake = 80;
+  delay(intakeTime);
   launcher = 0;
   launcher2 = 0;
 }
 
+void intakeFunc(int time) {
+  intake = 127;
+  delay(time);
+}
+
 void flip(int time) {
-  intake = -127;
+  intake = -100;
   delay(time);
 }
 // Move the motors.
-void drive(int left, int forLeft, int right, int forRight, int velocityLeft, int velocityRight, bool flipVal) {
+void drive(int left, int forLeft, int right, int forRight, int velocityLeft, int velocityRight, bool flipVal, bool intakeVal) {
   left_mtr.move_absolute(left, velocityLeft);
   forward_left_mtr.move_absolute(forLeft, velocityLeft);
   right_mtr.move_absolute(right, velocityRight);
@@ -25,6 +30,10 @@ void drive(int left, int forLeft, int right, int forRight, int velocityLeft, int
 
   if (flipVal) {
     flip(2);
+  }
+
+  if (intakeVal) {
+    intakeFunc(2);
   }
 
   while (!((left_mtr.get_position() < (left + 5)) && (left_mtr.get_position() > (left - 5)))) {
@@ -53,63 +62,54 @@ void drive(int left, int forLeft, int right, int forRight, int velocityLeft, int
 void autonomous() {
   if (red == true) {
     if (front == true) {
-      // 780 - 800 is about 90degs
-
-      // flag and first cone
-      // shoot();
-
-      // Moves from a backwards position, hits first flag, rotates towards first cone
+      // Drive forwards and intakes a ball, flips cone
       if (autonomousMode == 1) {
-        drive(-3200, -3200, 3200, 3200, 200, 200, false);
-        delay(20);
-        drive(1800, 1800, -1800, -1800, 200, 200, false);
-        delay(20);
-        drive(-760, -760, -760, -760, 200, 200, false);
+        drive(3200, 3200, -3200, -3200, 150, 150, false, true);
+        intakeFunc(150);
         delay(20);
       }
 
-      // Drives into first and flips first cone
+      // Drives back and returns to starting position
       if (autonomousMode == 2) {
-        drive(1800, 1800, -1800, -1800, 200, 200, true);
+        drive(-3400, -3400, 3400, 3400, 180, 180, false, false);
         delay(20);
-        flip(1000);
+        flip(50);
       }
 
-      // Rotates and move towards platform
+
       if (autonomousMode == 3) {
-          drive(760, 760, 760, 760, 200, 200, false);
-          delay(20);
-          drive(1800, 1800, -1800, -1800, 200, 200, false);
+        drive(700, 700, -700, -700, 200, 200, false, false);
+        delay(20);
+        drive(700, 700, 700, 700, 150, 150, false, false);
       }
 
       // Rotate towards cone and flips cone
       if (autonomousMode == 4) {
-          drive(-760, -760, -760, -760, 200, 200, false);
-          delay(20);
-          drive(1800, 1800, -1800, -1800, 200, 200, true);
-          flip(1000);
+        shoot(800, 250);
+        delay(20);
       }
 
-      // Moves back and rotates
-      if (autonomousMode == 5) {
-        drive(-5400, -5400, 5400, 5400, 200, 200, false);
-        delay(20);
-        drive(760, 760, -760, -760, 200, 200, false);
-        delay(20);
 
+      if (autonomousMode == 5) {
+        shoot(4000, 1000);
+        delay(20);
       }
 
       // Moves towards platform and rotates towards platform
       if (autonomousMode == 6) {
-          drive(1800, 1800, -1800, -1800, 200, 200, false);
-          delay(20);
-          drive(-760, -760, 760, 760, 200, 200, false);
-          delay(20);
+        drive(1800, 1800, -1800, -1800, 200, 200, false, false);
+        delay(20);
+        drive(-750, -750, -750, -750, 200, 200, false, false);
       }
 
       // Parks
       if (autonomousMode == 7) {
-          drive(4500, 4500, -4500, -4500, 200, 200, false);
+        drive(4200, 4200, -4200, -4200, 200, 200, false, false);
+        delay(20);
+      }
+
+      if (autonomousMode == 8) {
+
       }
 
 
